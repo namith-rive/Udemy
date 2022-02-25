5c924c6220acb9e1fadd396527ae046f287f85e6
Yuji <westnamith@gmail.com>
2022-02-25 18:06:55 +0900

    Push from Pycharm - Udemy　シリコンバレーエンジニアによるPython学習 sec5

--- SV_lesson_Sec6.py
+++ SV_lesson_Sec6.py
@@ -26,39 +26,63 @@
 # except ImportError:
 #     from lesson_package.tools import  utilis
 # utilis.say_twice('word')
+#
+# import  builtins
+# builtins.print()
+# ranking ={
+#     'A':100,
+#     'B':85,
+#     'C':95
+# }
+# r = ranking.get('B')
+# print(r)
+# print(sorted(ranking,key=ranking.get,reverse=True))
+#
+# print('################################')
+#
+# s="hbgvaohgaqw4e:gnao:gagrgq;vhzsdv"
+#
+# d={}
+# for c in s:
+#     if c not in d:
+#         d[c]=0
+#     d[c] += 1
+# print(d)
+#
+# d={}
+# for c in s:
+#     d.setdefault(c,0)
+#     d[c] += 1
+# print(d)
+#
+# from collections import defaultdict
+# d= defaultdict(int)
+# for c in s:
+#     d[c] +=1
+# print(d)
+#
+# print(d['h'])
 
-import  builtins
-builtins.print()
-ranking ={
-    'A':100,
-    'B':85,
-    'C':95
-}
-r = ranking.get('B')
-print(r)
-print(sorted(ranking,key=ranking.get,reverse=True))
 
-print('################################')
+# from termcolor import colored
+# print('test')
+# print(colored('test','red'))
+# print(help(colored))
+#
+# print('################')
+#
+# import collections
+# import  os
+# import sys # アルファベット順で書く
+#
+# import termcolor # 3rdパーティ製パッケージは一行開ける
+#
+# print(collections.__file__)
+# print(termcolor.__file__)
+#
+# print(sys.path)
+print('################')
 
-s="hbgvaohgaqw4e:gnao:gagrgq;vhzsdv"
+import config
 
-d={}
-for c in s:
-    if c not in d:
-        d[c]=0
-    d[c] += 1
-print(d)
-
-d={}
-for c in s:
-    d.setdefault(c,0)
-    d[c] += 1
-print(d)
-
-from collections import defaultdict
-d= defaultdict(int)
-for c in s:
-    d[c] +=1
-print(d)
-
-print(d['h'])
+print(__name__)

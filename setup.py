28aac8104a9cafc67e7e3ad6526cb838affbd968
Yuji <westnamith@gmail.com>
2022-03-06 18:22:02 +0900

    Push from Pycharm - Udemy　シリコンバレーエンジニアによるPython学習 sec19

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SamplePythonPrograming',
+    version='1.0.0',
+    packages=['lesson_package', 'lesson_package.talk', 'lesson_package.tools'],
+    url='',
+    license='',
+    author='yuji',
+    author_email='',
+    description='pythonProject_UdemySV'
+)

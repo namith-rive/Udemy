250931e95dc6795a290628ade0f95ca08af7acf0
Yuji <westnamith@gmail.com>
2022-03-07 02:24:22 +0900

    Push from Pycharm - Udemy　シリコンバレーエンジニアによるPython学習 sec16-暗号化

--- SV_lesson_sec16.py
+++ SV_lesson_sec16.py
@@ -1,36 +1,66 @@
-import string
-import random
+# import string
+# import random
+#
+# from Crypto.Cipher import AES
+# from  Crypto.PublicKey import RSA
+#
+# #print(AES.block_size)
+# #print(string.ascii_letters)
+# key = ''.join(
+#      random.choice(string.ascii_letters) for _ in range(AES.block_size))
+# #print(key)
+#
+# iv = ''.join(
+#      random.choice(string.ascii_letters) for _ in range(AES.block_size))
+# #print(iv)
+#
+# plaintext = 'hello_world'
+# cipher = AES.new(key, AES.MODE_CBC,iv)
+# # #文字長が16文字の倍数になるように調整する
+# padding_length = AES.block_size - len(plaintext) % AES.block_size
+# plaintext += chr(padding_length) * padding_length
+#
+# cipher_text = cipher.encrypt(plaintext)
+# print(cipher_text)
+#
+# # print(padding_length)
+# # print(AES.block_size)
+# # print(len(plaintext) % AES.block_size)
+# # print(len(plaintext) )
+# # print( chr(padding_length) )
+# # print(plaintext)
+#
+# cipher2 = AES.new(key, AES.MODE_CBC, iv)
+# decrypted_text = cipher2.decrypt(cipher_text)
+# print(decrypted_text[-1])
+# print(decrypted_text[:-decrypted_text[-1]])
 
-from Crypto.Cipher import AES
-from  Crypto.PublicKey import RSA
+import base64
+import hashlib
+import os
 
-#print(AES.block_size)
-#print(string.ascii_letters)
-key = ''.join(
-     random.choice(string.ascii_letters) for _ in range(AES.block_size))
-#print(key)
 
-iv = ''.join(
-     random.choice(string.ascii_letters) for _ in range(AES.block_size))
-#print(iv)
+# print(hashlib.sha256(b'test').digest())
+# print(hashlib.sha256(b'test').digest())
 
-plaintext = 'hello_world'
-cipher = AES.new(key, AES.MODE_CBC,iv)
-# #文字長が16文字の倍数になるように調整する
-padding_length = AES.block_size - len(plaintext) % AES.block_size
-plaintext += chr(padding_length) * padding_length
+user_name = 'user1'
+user_pass = 'password'
+db={}
 
-cipher_text = cipher.encrypt(plaintext)
-print(cipher_text)
+salt = base64.b64encode(os.urandom(32))
+print(salt)
 
-# print(padding_length)
-# print(AES.block_size)
-# print(len(plaintext) % AES.block_size)
-# print(len(plaintext) )
-# print( chr(padding_length) )
-# print(plaintext)
+def get_digest(password):
+    password = bytes(user_pass, 'utf-8')
+    digest = hashlib.sha256(salt + password).hexdigest()
+    for _ in range(1000):
+        digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
+    print(digest)
+    return digest
 
-cipher2 = AES.new(key, AES.MODE_CBC, iv)
-decrypted_text = cipher2.decrypt(cipher_text)
-print(decrypted_text[-1])
-print(decrypted_text[:-decrypted_text[-1]])
+db[user_name] = get_digest(user_pass)
+#print(db)
+def is_login(user_name, password):
+    return get_digest(password) == db[user_name]
+
+print(is_login(user_name, user_pass))
